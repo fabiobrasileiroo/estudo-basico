2fb63f7c84df3f511f861fe46b314238435b5867
fabiopc <fabiopc@debian-BULLSEYE-live-builder-AMD64>
2023-11-09 21:34:19 -0400

    renderizacao de lista

---
+++ src/app/components/list-render/list-render.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-render',
+  templateUrl: './list-render.component.html',
+  styleUrls: ['./list-render.component.css']
+})
+export class ListRenderComponent {
+  animals = [
+    {name: "Turca", type: "Dog"},
+    {name: "Tom", type: "Cat"},
+    {name: "Frida", type: "Dog"},
+    { name: "Bob", type: "horse"}
+  ];
+  anima = [
+    { name: 'Teste', type: "sla" },
+    { name: 'Alguma coisa', type: "foi" }
+  ];
+};
