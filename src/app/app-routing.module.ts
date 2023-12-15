9ee29898926b1572c70f23451c7c9b280851d3b3
Fábio Henrique Mendes Brasileiro <99563281+fabiobrasileiroo@users.noreply.github.com>
2023-12-15 18:05:28 -0300

    teste

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+//import { first } from 'rxjs';
+//import { ListService } from './services/list.service';
+import { Directive } from '@angular/core';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+const routes: Routes = [
+  {path: '', component: Directive},
+  {path: 'list', component:ListRenderComponent}
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
